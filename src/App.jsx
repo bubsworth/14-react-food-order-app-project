d70fc18cc3b196c55ecbd1cf7c1e9342d9f7d342
Beth Hubbard <bhubbard@yozu.co.uk>
2024-02-02 14:49:26 +0000

    Planning the App & Adding a First Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
-import logoImg from "../src/assets/logo.jpg";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
-      {/* <Modal open={errorUpdatingAvailableMeals} onClose={handleError}>
-        {errorUpdatingAvailableMeals && (
-          <Error
-            title="An error occurred!"
-            message={errorUpdatingAvailableMeals.message}
-            onConfirm={handleError}
-          />
-        )}
-      </Modal>
-
-      <Modal open={modalIsOpen} onClose={handleStopRemoveAvailableMealItem}>
-        <DeleteConfirmation
-          onCancel={handleStopRemoveAvailableMealItem}
-          onConfirm={handleRemoveAvailableMealItem}
-        />
-      </Modal> */}
-
-      <header>
-        <img src={logoImg} alt="" />
-        <h1>ReactFood</h1>
-      </header>
-      <main>
-        {/* {error && <Error title="An error occurred!" message={error.message} />}
-        {!error && (
-          <MealItems
-            title="I'd like to visit ..."
-            fallbackText="Select the places you would like to visit below."
-            isLoading={isFetching}
-            loadingText="Fetching available meals..."
-            meals={userPlaces}
-            onSelectPlace={handleStartRemovePlace}
-          />
-        )}
-
-        <AvailableMeals onSelectMealItem={handleSelectMealItem} /> */}
-      </main>
+      <Header />
+      <main></main>
     </>
   );
 }
