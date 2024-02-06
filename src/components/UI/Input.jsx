87f310285b879a46689a66ba3f6369c47754e1b4
bubsworth <bubbard96@hotmail.com>
2024-02-06 15:11:29 +0000

    Handling Form Submission & Validation

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,12 +1,10 @@
-import { forwardRef } from "react";
-
-const Input = forwardRef(function Input({ label, id, ...props }, ref) {
+function Input({ label, id, ...props }, ref) {
   return (
     <p className="control">
       <label htmlFor={id}>{label}</label>
       <input ref={ref} id={id} name={id} required {...props} />
     </p>
   );
-});
+}
 
 export default Input;
